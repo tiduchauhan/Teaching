c24ac7667483651d4f185e53e65ab39492bd8346
tiduchauhan <141713647+tiduchauhan@users.noreply.github.com>
2024-07-04 09:44:57 +0530

    Create udit.js

---
+++ ans1/udit.js
@@ -0,0 +1,10 @@
+let myName = "udit chauhan";
+console.log("Name:", myName);
+
+let myAge = 25; 
+let isStudent = true; 
+let favoriteHobbies = ["Reading", "observing", "Hiking"]; 
+console.log("Type of myName:", typeof myName);
+console.log("Type of myAge:", typeof myAge);
+console.log("Type of isStudent:", typeof isStudent);
+console.log("Type of favoriteHobbies:", typeof favoriteHobbies);
